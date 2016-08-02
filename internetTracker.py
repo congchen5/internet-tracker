37c16cb99b38c03080b1495579c6c67b15f0f7cf
Cong Chen <congchen5@gmail.com>
2016-08-02 00:51:58 -0700

    Added handling for SpeedTest Exceptions so that the program won't stop.

--- internetTracker.py
+++ internetTracker.py
@@ -8,22 +8,30 @@ from datetime import datetime
 from pytz import timezone
 
 SPEED_TEST_OUTPUT_REGEX_PATTERN = 'Ping:\s([\d\.]+)\s\w+\nDownload:\s([\d\.]+)\s[\w\/]+\nUpload:\s([\d\.]+)'
-INTERVAL_TIME_SEC = 3600 # 1 hour
+INTERVAL_TIME_SEC = 600 # 10 minutes
 
 def RunSpeedTest():
   try:
     #return 'Ping: 12.34 ms\nDownload: 23.45 Mbit/s\nUpload: 10.1 Mbit/s'
-    return subprocess.check_output(["speedtest-cli", "--simple"])
+    speedResult = subprocess.check_output(['speedtest-cli', '--simple'])
+  except subprocess.CalledProcessError as e:
+    print('Error in running the speed test.')
+    print('Command \'{}\' return with error (code {}): {}'.format(
+        e.cmd, e.returncode, e.output))
+    return -1
+
   except:
-    print("Error in running the speed test.");
-    raise
+    print('General Error in running the speed test.')
+    return -1
+
+  return speedResult;
 
 def ParseSpeedTestOutput(output):
   matches = re.match(SPEED_TEST_OUTPUT_REGEX_PATTERN, output)
   try:
-    print("Ping: " + matches.group(1))
-    print("Download: " + matches.group(2))
-    print("Upload: " + matches.group(3))
+    print('Ping: ' + matches.group(1))
+    print('Download: ' + matches.group(2))
+    print('Upload: ' + matches.group(3))
 
     ping = matches.group(1)
     download = matches.group(2)
@@ -42,13 +50,19 @@ def ContinuousTesting():
   while True:
     pst = timezone('US/Pacific')
     print('SpeedTest started at: ' + str(datetime.now(pst)))
-    runOutput = ParseSpeedTestOutput(RunSpeedTest())
+    speedTestResult = RunSpeedTest()
 
-    # Append to the csv file.
-    with open('speedData.csv', 'a', 0) as csvfile:
-      csvWriter = csv.writer(csvfile, delimiter=' ')
-      csvWriter.writerow([time.time(), str(datetime.now(pst)), runOutput[0],
-          runOutput[1], runOutput[2]])
+    # Only parse and export data if the speedTest did not fail.
+    if (speedTestResult != -1):
+      runOutput = ParseSpeedTestOutput(RunSpeedTest())
+
+      # Append to the csv file.
+      with open('speedData.csv', 'a', 0) as csvfile:
+        csvWriter = csv.writer(csvfile, delimiter=' ')
+        csvWriter.writerow([time.time(), str(datetime.now(pst)), runOutput[0],
+            runOutput[1], runOutput[2]])
+    else:
+      print('Skipping SpeedTest due to failed test.')
 
     time.sleep(INTERVAL_TIME_SEC)
 
