a93fe66928452a1da33d55b11309fb43aba7ebc3
Cong Chen <congchen5@gmail.com>
2016-08-17 00:03:35 -0700

    Polling is now handled by a class. Also added debug flag.

--- internetTracker.py
+++ internetTracker.py
@@ -21,36 +21,60 @@ from pprint import pprint
 #   - offset: TODO
 # }
 
-SPEED_TEST_OUTPUT_REGEX_PATTERN = (
+class PollingSpeedTest:
+  """A class that allows you to poll speedTests and export the result to csv"""
+  speedTestOutputRegex = (
     'Ping:\s([\d\.]+)\s\w+\nDownload:\s([\d\.]+)\s[\w\/]+\nUpload:\s([\d\.]+)')
-INTERVAL_TIME_SEC = -1
 
-class PollSpeedTest:
-  def __init__(self, interval):
+  def __init__(self, debug, interval):
+    self.debug = debug
     self.interval = interval
 
     # Whether polling is active or not
-    self.active = false
+    self.active = False
 
   def Start(self):
     self.active = True
+    self.Poll()
 
   def Stop(self):
     self.active = False
 
   def Poll(self):
-    print('ToDo')
+    pst = timezone('US/Pacific')
+    print('SpeedTest started at: ' + str(datetime.now(pst)))
+    speedTestResult = self._RunSpeedTest()
+
+    # Only parse and export data if the speedTest did not fail.
+    if (speedTestResult != -1):
+      runOutput = self._ParseSpeedTestOutput(self._RunSpeedTest())
+
+      # Append to the csv file.
+      with open('speedData.csv', 'a', 0) as csvfile:
+        csvWriter = csv.writer(csvfile, delimiter=' ')
+        csvWriter.writerow([time.time(), str(datetime.now(pst)), runOutput[0],
+            runOutput[1], runOutput[2]])
+    else:
+      print('Skipping SpeedTest due to failed test.')
+
+    # If polling is active, all Poll again after waiting interval amount of time
+    if self.active:
+      time.sleep(self.interval)
+      self.Poll()
+    else:
+      print('Polling stopped.')
 
   def _RunSpeedTest(self):
     try:
-      # return 'Ping: 12.34 ms\nDownload: 23.45 Mbit/s\nUpload: 10.1 Mbit/s'
-      speedResult = subprocess.check_output(['speedtest-cli', '--simple'])
+      if self.debug:
+        return 'Ping: 11.11 ms\nDownload: 22.22 Mbit/s\nUpload: 33.3 Mbit/s'
+      else:
+        speedResult = subprocess.check_output(['speedtest-cli', '--simple'])
     except subprocess.CalledProcessError as e:
       print('Error in running the speed test.')
       print('Command \'{}\' return with error (code {}): {}'.format(
           e.cmd, e.returncode, e.output))
       return -1
-
     except:
       print('General Error in running the speed test.')
       return -1
@@ -58,7 +82,7 @@ class PollSpeedTest:
     return speedResult;
 
   def _ParseSpeedTestOutput(self, output):
-    matches = re.match(SPEED_TEST_OUTPUT_REGEX_PATTERN, output)
+    matches = re.match(self.speedTestOutputRegex, output)
     try:
       print('Ping: ' + matches.group(1))
       print('Download: ' + matches.group(2))
@@ -77,48 +101,29 @@ class PollSpeedTest:
 def ReadSettingsConfig():
   with open('settings.config') as settings_file:
     data = json.load(settings_file)
-  # pprint(data)
+  #pprint(data)
 
-  global INTERVAL_TIME_SEC
+  debug = False
+  interval = -1
+
   if 'interval' in data:
-    INTERVAL_TIME_SEC = data['interval']
+    interval = data['interval']
 
-def CheckConfigValues():
-  if INTERVAL_TIME_SEC == -1:
+  # Make sure an interval time is supplied.
+  if interval == -1:
     raise Exception('Please supply an interval time in settings.config.')
 
-# TODO: define a class that sets the interval and has a start and stop. Also
-# make it account for the time it takes to run the test.
-def ContinuousTesting():
-  print('Interval Time: ' + str(INTERVAL_TIME_SEC) + ' sec.')
+  if 'debug' in data:
+    debug = data['debug']
 
-  while True:
-    pst = timezone('US/Pacific')
-    print('SpeedTest started at: ' + str(datetime.now(pst)))
-    speedTestResult = RunSpeedTest()
-
-    # Only parse and export data if the speedTest did not fail.
-    if (speedTestResult != -1):
-      runOutput = ParseSpeedTestOutput(RunSpeedTest())
-
-      # Append to the csv file.
-      with open('speedData.csv', 'a', 0) as csvfile:
-        csvWriter = csv.writer(csvfile, delimiter=' ')
-        csvWriter.writerow([time.time(), str(datetime.now(pst)), runOutput[0],
-            runOutput[1], runOutput[2]])
-    else:
-      print('Skipping SpeedTest due to failed test.')
-
-    time.sleep(INTERVAL_TIME_SEC)
+  return [debug, interval]
 
 def main():
   # First, read in the settings.config
-  ReadSettingsConfig()
+  configs = ReadSettingsConfig()
 
-  # Make sure all values are properly set from config file
-  CheckConfigValues()
-
-  ContinuousTesting()
+  pollingSpeedTest = PollingSpeedTest(configs[0], configs[1])
+  pollingSpeedTest.Start()
 
 if __name__ == '__main__':
   # execute only if run as a script
